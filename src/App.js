66ea7b08d149f38cf6aac58e660bc08062e8615b
Leonardo Palavecino <leo43161301gmail.com>
2020-10-03 11:07:53 -0300

    Se empezo a trabajar en el portfolio

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import SobreMi from "./Components/SobreMi";
 import Experiencia from "./Components/Experiencia";
 import Habilidades from "./Components/Habilidades";
 import Contactame from "./Components/Contactame";
+import Portafolio from "./Components/Portafolio";
 
 function App() {
   return (
-    <div>
+    <div className="owsla">
       <section className="d-flex px-3 py-2">
         <div className="col-7 p-0 shadow">
           <Headers></Headers>
@@ -33,6 +34,15 @@ function App() {
         </div>
         <div className="col-5 pl-3 pr-0 shadow">
           <Contactame></Contactame>
+        </div>
+      </section>
+      <section className="w-100">
+        <div className="text-center">
+          <h1>Portfolio</h1>
+          <hr></hr>
+        </div>
+        <div className="container-fluid">
+          <Portafolio></Portafolio>
         </div>
       </section>
     </div>
---
+++ src/Components/Portafolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Diseños from './Diseños';
+
+const Portafolio = () => {
+    return (
+        <div className="w-100 d-flex flex-wrap">
+            <Diseños></Diseños>
+        </div>
+    );
+};
+
+export default Portafolio;
