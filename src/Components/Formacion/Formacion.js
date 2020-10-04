af1991b41a89db1999f204932be7c55139d8b543
Leonardo Palavecino <leo43161301gmail.com>
2020-10-04 17:23:02 -0300

    Se creo el maquetado basico de una card de formacion

--- src/Components/Formacion/Formacion.js
+++ src/Components/Formacion/Formacion.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import FormacionCard from "./FormacionCard";
 
 const Formacion = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="owsla bg-blue shadow">
+      <div className="w-100 text-white">
+        <h2 className="text-center">Formacion Academica</h2>
+      </div>
+      <FormacionCard></FormacionCard>
+    </div>
+  );
 };
 
 export default Formacion;
--- src/Components/Formacion/FormacionCard.js
+++ src/Components/Formacion/FormacionCard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const FormacionCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="bg-blue text-white d-flex">
+      <div className="col-4 p-3 d-flex flex-column bg-black justify-content-center align-items-start border-right border-top shadow">
+        <h6 className="font-weight-light">2020</h6>
+        <h5 className="font-weight-light">Monteros</h5>
+      </div>
+      <div className="col-8 p-3 border-top">
+        <h3 className="font-weight-bold">Programador FullStack</h3>
+        <p>Un curso de programacion FullStack donde aprendí a manejar el frontend y backend de un sistema orientado en paguinas web, ademas de enseñarnos a trabajar en una metodologia Scrum y aprender a usar herramientas verdareramente utiles como Slack | Trello | Git | Drive | Zoom y muchas mas...</p>
+      </div>
+    </div>
+  );
 };
 
 export default FormacionCard;
