899733e60f493a1f1ae04b1f1a179155e2b3d5e2
Leonardo Palavecino <leo43161301gmail.com>
2020-10-06 20:31:44 -0300

    Se añadio un pequeño detalle

--- src/Components/Headers.js
+++ src/Components/Headers.js
@@ -17,7 +17,7 @@ const Headers = () => {
         <div className="w-100 py-2 pl-3 bg-blue text-white border-bottom">
           <h1 className="m-0">Leonardo Palavecino</h1>
           <h5 className="font-weight-light font-small p-1 bg-black">
-            <em>Programador Web</em>
+            <em>Programador Web (Junior)</em>
           </h5>
           <div className="d-flex justify-content-between">
             <div className="d-flex">
