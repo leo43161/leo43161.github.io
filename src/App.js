46975a79559fc7c4f1933e07911514d95356b751
Leonardo <leo43161301@gmail.com>
2021-01-20 15:50:42 -0300

    Se dejo solo la parte del portfolio y se comento las demas secciones

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Assets/css/font.css";
-import Headers from "./Components/Headers";
+/* import Headers from "./Components/Headers";
 import SobreMi from "./Components/SobreMi";
 import Experiencia from "./Components/Experiencia/Experiencia";
 import Habilidades from "./Components/Habilidades";
-import Contactame from "./Components/Contactame";
+import Contactame from "./Components/Contactame"; */
 import Portafolio from "./Components/Portafolio/Portafolio";
-import Formacion from "./Components/Formacion/Formacion";
+/* import Formacion from "./Components/Formacion/Formacion"; */
 
 function App() {
   return (
     <div className="owsla">
-      <section className="d-flex flex-wrap px-3 py-2">
+      {/* <section className="d-flex flex-wrap px-3 py-2">
         <div className="col-lg-7 col-12 p-0 shadow">
           <Headers></Headers>
         </div>
@@ -36,7 +36,7 @@ function App() {
         <div className="col-lg-5 col-12 pl-0 pl-lg-3 pr-0 shadow mt-3 mt-lg-0">
           <Contactame></Contactame>
         </div>
-      </section>
+      </section> */}
       <section className="w-100 mt-2">
         <div className="text-center px-3">
           <h1>Portfolio</h1>
